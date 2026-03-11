b14bb56163d8410a90ff57d2f2740fbd884e2d94
YorkshireYeti <lukemartin2804@gmail.com>
2026-03-11 21:53:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,74 +1,12 @@
-const map = L.map("map").setView([25,40],3);
+const express = require("express");
+const path = require("path");
 
-L.tileLayer("https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png",{
-maxZoom:20
-}).addTo(map);
+const app = express();
 
-let markers=[];
+app.use(express.static(path.join(__dirname,"public")));
 
-function clearMarkers(){
-markers.forEach(m=>map.removeLayer(m));
-markers=[];
-}
+const PORT = process.env.PORT || 3000;
 
-function createPlaneIcon(flight){
-
-return L.divIcon({
-className:"",
-html:`
-
-<div style="text-align:center">
-<div style="
-background:black;
-color:white;
-padding:3px 8px;
-border-radius:4px;
-font-size:13px;
-margin-bottom:4px;
-display:inline-block;">
-${flight}
-</div>
-<div style="color:red;font-size:34px;">✈</div>
-</div>
-`,
-iconSize:[60,60],
-iconAnchor:[30,30]
+app.listen(PORT,()=>{
+console.log("Flight tracker running");
 });
-
-}
-
-async function updateFlights(){
-
-const res = await fetch("https://opensky-network.org/api/states/all");
-
-const data = await res.json();
-
-clearMarkers();
-
-let flights = ["UAE28","UAE376","UAE375","UAE27"];
-
-data.states.forEach(s=>{
-
-let callsign = s[1] ? s[1].trim() : "";
-
-if(!flights.includes(callsign)) return;
-
-let lat = s[6];
-let lon = s[5];
-
-if(!lat || !lon) return;
-
-let marker = L.marker(
-[lat,lon],
-{icon:createPlaneIcon(callsign.replace("UAE","EK"))}
-).addTo(map);
-
-markers.push(marker);
-
-});
-
-}
-
-updateFlights();
-
-setInterval(updateFlights,10000);
